9c73e62af7d11d33ab2f9d09d404300080fe3e49
Kunle <OS409@live.mdx.ac.uk>
2023-11-05 19:32:08 +0000

    added lessons.js

---
+++ lessons.js
@@ -0,0 +1,20 @@
+let lessons = [
+    {
+        "id": 1001,
+        "title": "Cat Food, 25lb bag",
+        "description": "A 25 pound bag of <em>irresistible</em>, organic goodness for your cat.",
+        "price": 2000,
+        "image": "catfood.jpg",
+        "availableInventory": 10,
+        "rating": 3
+    },
+    {
+        "id": 1002,
+        "title": "Yarn",
+        "description": "Yarn your cat can play with for a very <strong>long</strong> time",
+        "price": 1000,
+        "image": "yarn.jpg",
+        "availableInventory": 8,
+        "rating": 5
+    },
+]
