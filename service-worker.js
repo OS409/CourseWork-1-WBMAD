b447cc3995b3df488172180356ca9ca7fd3bd20e
Kunle <OS409@live.mdx.ac.uk>
2024-02-29 16:42:13 +0000

    replaced with server calls

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,20 @@
 var cacheName = "classpurchase";
 var cacheFiles = [
     "index.html",
-    "images/art.png",
-    "images/assyrian_flag.png",
-    "images/biology.png",
-    "images/chemistry.png",
-    "images/classics.png",
-    "images/computer.png",
-    "images/english.png",
-    "images/geography.png",
-    "images/icon-32.png",
-    "images/icon-512.png",
-    "images/maths.png",
-    "images/music.png",
-    "images/physics.png",
-    "images/science.png"//shouuld thie be changed?
+    "http://testapp-env.eba-26mfnh5p.eu-west-2.elasticbeanstalk.com/art.png",
+    "http://testapp-env.eba-26mfnh5p.eu-west-2.elasticbeanstalk.com/assyrian_flag.png",
+    "http://testapp-env.eba-26mfnh5p.eu-west-2.elasticbeanstalk.com/biology.png",
+    "http://testapp-env.eba-26mfnh5p.eu-west-2.elasticbeanstalk.com/chemistry.png",
+    "http://testapp-env.eba-26mfnh5p.eu-west-2.elasticbeanstalk.com/classics.png",
+    "http://testapp-env.eba-26mfnh5p.eu-west-2.elasticbeanstalk.com/computer.png",
+    "http://testapp-env.eba-26mfnh5p.eu-west-2.elasticbeanstalk.com/english.png",
+    "http://testapp-env.eba-26mfnh5p.eu-west-2.elasticbeanstalk.com/geography.png",
+    "http://testapp-env.eba-26mfnh5p.eu-west-2.elasticbeanstalk.com/icon-32.png",
+    "http://testapp-env.eba-26mfnh5p.eu-west-2.elasticbeanstalk.com/icon-512.png",
+    "http://testapp-env.eba-26mfnh5p.eu-west-2.elasticbeanstalk.com/maths.png",
+    "http://testapp-env.eba-26mfnh5p.eu-west-2.elasticbeanstalk.com/music.png",
+    "http://testapp-env.eba-26mfnh5p.eu-west-2.elasticbeanstalk.com/physics.png",
+    "http://testapp-env.eba-26mfnh5p.eu-west-2.elasticbeanstalk.com/science.png"//shouuld thie be changed?
 ];
 
 self.addEventListener("install", function(e) {
