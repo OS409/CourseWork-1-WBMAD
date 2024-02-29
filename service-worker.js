d56a926ba27165aa3fdaa18e595384e6daddbc6d
Kunle <OS409@live.mdx.ac.uk>
2024-02-29 15:24:00 +0000

    added service workers

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+var cacheName = "lessonpurchase";
+var cacheFiles = [
+    "index.html",
+    "images/art.png",
+    "images/biology.png",
+    "images/chemistry.png",
+    "images/computer.png",
+    "images/english.png",
+    "images/classics.png",
+    "images/geography.png",
+    "images/maths.png",
+    "images/music.png",
+    "images/physics.png",
+    "images/science.png",
+    "images/assyrian_flag.png"
+];
+
+self.addEventListener("install", function(e) {
+    console.log("[Service Worker] Install");
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log("[Service Worker] Caching files");
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
